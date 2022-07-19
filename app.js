2c9ec89f4e70301c5ad7a9fe0c7fd3639ca6b976
Brenda Liu <brenda.liu24@gmail.com>
2022-07-19 15:54:40 -0700

    make form and axios get

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Let's get this party started!");
+
+const $form = $('#search-giphy');
+const $searchButton = $('#search-button');
+
+function getFormVal(e) {
+  e.preventDefault();
+  const formVal = $('#form-input').val();
+  console.log(formVal);
+}
+
+$searchButton.on('click', getFormVal);
+
+
+async function getSearchValue() {
+  let response = await axios.get(" http://api.giphy.com/v1/gifs/search",
+    { params: { p: formVal } });
+}
