9aec3d98d8372d23429d0783497eecca9e61ca18
Margaret Sun <hi.margaret.sun@gmail.com>
2022-07-19 16:50:37 -0700

    added function to append giphy

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+"use strict";
 console.log("Let's get this party started!");
 
 const $form = $('#search-giphy');
 const $searchButton = $('#search-button');
 const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 
+
+/** Takes in event and returns JSON of searched value*/
 async function getSearchValue(e) {
   e.preventDefault();
   const formVal = $('#form-input').val();
   let response = await axios.get(" http://api.giphy.com/v1/gifs/search",
     { params: { q: formVal, api_key: API_KEY} });
-  console.log(response.data);
+  const giphy = response.data.data[0].images.original.url
+  return appendGiphy(giphy)
 }
 
 $searchButton.on('click', getSearchValue);
+
+
+
+
+ function appendGiphy(giphy){
+    $("#new-giphy").append($(`<img src= ${giphy}>`))
+ }
