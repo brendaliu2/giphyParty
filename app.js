968c34c9af90e25b3574c144446e5a65d7c2bfa9
Margaret Sun <hi.margaret.sun@gmail.com>
2022-07-19 17:13:10 -0700

    refactored code and added docstrings

--- app.js
+++ app.js
@@ -6,28 +6,36 @@ const $searchButton = $('#search-button');
 const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 
 
-/** Takes in event and returns JSON of searched value*/
-async function getSearchValue(e) {
+/** Takes in event and calls getFormVal*/
+function getSearchValue(e) {
   e.preventDefault();
   const formVal = $('#form-input').val();
+  return getFormVal(formVal);
+}
+
+/**Takes in a string
+ * and finds JSON object and finds the first giphy
+ * then calls appendGiphy */
+async function getFormVal(formVal) {
   let response = await axios.get(" http://api.giphy.com/v1/gifs/search",
-    { params: { q: formVal, api_key: API_KEY} });
+    { params: { q: formVal, api_key: API_KEY } });
   const giphy = response.data.data[0].images.original.url;
   return appendGiphy(giphy);
 }
 
+
 $searchButton.on('click', getSearchValue);
 
 
+/** Adds giphy onto DOM*/
+function appendGiphy(giphy) {
+  $("#new-giphy").append($(`<img src= ${giphy}>`));
+}
 
-
- function appendGiphy(giphy){
-    $("#new-giphy").append($(`<img src= ${giphy}>`));
- }
-
- function emptyGiphy(e) {
+/** Clears giphys from DOM */
+function emptyGiphy(e) {
   let $giphyContainer = $("#new-giphy");
   $giphyContainer.empty();
- }
+}
 
- $("#remove-button").on('click', emptyGiphy);
+$("#remove-button").on('click', emptyGiphy);
